1cf2aee2743dc67cca42ae4096fe6fe7bb88117e
Mudusar Arshad <hafizmudusar37@gmail.com>
2024-05-18 18:22:09 +0500

    bg fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={exo.className} style={{ background: "#f0e8ca" }}>
         <main className="home">
+          {/* <div> */}
           <Web3ModalProvider initialState={initialState}>
             <ContextAPI>
               <Toaster />
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
               {children}
             </ContextAPI>
           </Web3ModalProvider>
+          {/* </div> */}
         </main>
       </body>
     </html>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,19 +5,21 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"], weight: "600" });
 import { useAccount } from "wagmi";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useRouter } from "next/navigation";
 const Navbar = () => {
   const { address, isConnected } = useAccount();
   const { open, close } = useWeb3Modal();
-
+  const router = useRouter();
   return (
     <div className="flex justify-between items-center 2xl:px-[5%]   max-2xl:px-[100px] py-[5px] max-lg:px-[50px] max-md:px-[20px] ">
       <button className="bg-[#DF8B24] hover:bg-[#DF8B24]/90 text-[#f9eba7] font-[600px] invisible max-sm:hidden text-[14px] py-[10px] px-[20px] rounded-[50px]">
         Connect
       </button>
       <Image
+        onClick={() => router.push("/")}
         height={145}
         width={145}
-        className="max-sm:w-[68px] max-sm:h-[68px] max-lg:w-[100px] max-lg:h-[100px]"
+        className="max-sm:w-[68px] cursor-pointer max-sm:h-[68px] max-lg:w-[100px] max-lg:h-[100px]"
         src={"/logo.svg"}
         alt="logo"
       ></Image>
